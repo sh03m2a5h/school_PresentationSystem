c165a7735bbdcc79c2ae7f2c8471044adca33c0f
sakakura <sakakural+github@gmail.com>
2019-06-26 19:33:19 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,73 @@
+const puppeteer = require('puppeteer');
+const datas = require('./data.js');
+
+(async () => {
+    const browser = await puppeteer.launch({ headless: false, args: ['--disable-infobar'], timeout: 15000 });
+    const page = await browser.newPage(); const { targetInfos: [{ targetId }] } = await browser._connection.send(
+        'Target.getTargets'
+    )
+
+    const { windowId } = await browser._connection.send(
+        'Browser.getWindowForTarget', {
+            targetId
+        }
+    )
+
+    const myresize = async (c, wid, w, h) => {
+        await page.setViewport({ width: w, height: h })
+        await c.send('Browser.setWindowBounds', {
+            bounds: {
+                height: h,
+                width: w
+            },
+            windowId: wid
+        })
+    }
+    await page.setViewport({ width: 1280, height: 720 });
+    await page.goto('https://office.com');
+
+    {   /* ログイン処理 */
+        await page.waitForSelector('#hero-banner-sign-in-to-office-365-link');
+        await page.click('#hero-banner-sign-in-to-office-365-link');
+        await page.waitForSelector('input[type="email"]');
+        await page.type('input[type="email"]', datas.user.id);
+        await page.click('#idSIButton9');
+        await page.waitFor(2000);
+        await page.waitForSelector('input[type="password"]');
+        await page.type('input[type="password"]', datas.user.passwd);
+        await page.click('#idSIButton9');
+        await page.waitForSelector('#idBtn_Back');
+        await page.click('#idBtn_Back');
+    }
+
+    const ppURL = await (
+        /**
+         * OneDrive探索処理
+         * @returns {string}
+         */
+        async (text) => {
+            await page.goto('https://onedrive.live.com');
+            await page.waitForSelector('div[data-automationid="ドキュメント"]');
+            await page.click('div[data-automationid="ドキュメント"]');
+            await page.waitForSelector(`div[data-automationid*="${text}"]`);
+            return await page.evaluate((text) => {
+                return document.querySelector(`div[data-automationid*="${text}"] > a`).href
+            }, text);
+        })('Quantum Computer.pptx');
+
+    myresize(browser._connection, windowId,datas.display.width,datas.display.height)
+    {   /* PowerPointスライドショー表示処理 */
+        await page.goto(ppURL);
+        await page.waitForSelector('iframe[name="wac_frame"]');
+        const frame = await page.frames().find(f => f.name() === 'wac_frame');
+        await frame.waitFor(5000);
+        await frame.waitForSelector(`button[data-automation-id*="View"]`);
+        await frame.click(`button[data-automation-id="View"]`);
+        await frame.waitFor(2000);
+        await frame.waitForSelector(`button[data-automation-id="PlayFromBeginning"]`);
+        await frame.click(`button[data-automation-id="PlayFromBeginning"]`);
+    }
+    await page.waitFor(1000000);
+
+    await browser.close();
+})();
